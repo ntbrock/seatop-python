d5c38f496193987536d96723674d5b7352080660
Taylor Brockman <taylor.brockman@gmail.com>
2021-01-30 07:10:45 -0500

    test-gpio - Basic led switch demo

---
+++ test-gpio/test-switch-led.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# https://learn.adafruit.com/circuitpython-digital-inputs-and-outputs/digital-outputs
+
+import board
+import pulseio
+import time
+
+import digitalio
+
+led = digitalio.DigitalInOut(board.D21)
+led.direction = digitalio.Direction.OUTPUT
+
+switch = digitalio.DigitalInOut(board.D20)
+switch.direction = digitalio.Direction.INPUT
+
+led.value = True
+time.sleep(0.5)
+led.value = False
+
+while True:
+	s = switch.value
+	led.value = not s
+	time.sleep(0.1)
+
+
+
+
+
